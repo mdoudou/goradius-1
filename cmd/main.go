5070bb62d20bd54d803548afb39cc5aed3b79f6c
waso <waso@wasodeMacBook-Pro.local>
2019-05-27 14:19:02 +0800

    修改并发代码

--- cmd/main.go
+++ cmd/main.go
@@ -11,8 +11,7 @@ import (
 )
 
 func main() {
-	var ch chan int
-	ch = make(chan int)
+
 	logfile, err := os.OpenFile("./goradius.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		os.Exit(-1)
@@ -23,7 +22,6 @@ func main() {
 	log.SetFlags(log.Llongfile | log.Ldate | log.Ltime)
 
 	goradius.DbLive() //判断数据库是否存在
-
 	udpaddr, err := net.ResolveUDPAddr("udp", ":1812")
 	if err != nil {
 		fmt.Println(err)
@@ -33,7 +31,7 @@ func main() {
 	if err2 != nil {
 		log.Println(err2)
 	}
-	go goradius.Server(udpconn)
 
-	<-ch
+	goradius.Server(udpconn)
+
 }
--- server.go
+++ server.go
@@ -6,39 +6,44 @@ import (
 )
 
 func Server(conn *net.UDPConn) {
-	for {
-		defer conn.Close()
-		buf := make([]byte, 4096)
 
+	for {
+		buf := make([]byte, 4096)
 		_, udpaddr, err := conn.ReadFromUDP(buf)
 		if err != nil {
 			return
 		}
-		go func() {
-			ipp := udpaddr.IP.String()
-			secret := GetSecretAndDiff(ipp)
-			if secret == nil {
-				return
-			}
-			pakage, _ := Parse(buf, secret)
-			userName := UserName_GetString(pakage)
-			uerPassword := UserPassword_GetString(pakage)
-			//打印用户名密码和NAS IP
-			log.Println("username=", userName, "userpassword=", uerPassword, "nasIP=", ipp)
 
-			if GetUserPasswd(uerPassword) == uerPassword {
+		ipp := udpaddr.IP.String()
+		secret := GetSecretAndDiff(ipp)
+		if secret == nil {
+			return
+		}
+		pakage, _ := Parse(buf, secret)
+		userName := UserName_GetString(pakage)
+		uerPassword := UserPassword_GetString(pakage)
+
+		go func() {
+			defer conn.Close()
+			if GetUserPasswd(userName) == uerPassword {
 				res := pakage.Response(CodeAccessAccept)
 				var vl = []byte{'o', 'k'}
 				ReplyMessage_Add(res, vl)
 				udpp, _ := res.Encode()
 				conn.WriteToUDP(udpp, udpaddr)
+				//打印用户名密码和NAS IP
+				log.Println("username=", userName, "userpassword=", uerPassword, "nasIP=", ipp, "OK")
+
 			} else {
 				res := pakage.Response(CodeAccessReject)
 				var vl = []byte{'n', 'o'}
 				ReplyMessage_Add(res, vl)
 				udpp, _ := res.Encode()
 				conn.WriteToUDP(udpp, udpaddr)
+				//打印用户名密码和NAS IP
+				log.Println("username=", userName, "userpassword=", uerPassword, "nasIP=", ipp, "NO")
 			}
+
 		}()
 	}
 }
